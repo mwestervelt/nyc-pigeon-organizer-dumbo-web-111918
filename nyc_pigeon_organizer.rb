07ef50bf6894050a36ea07505229dac23740db58
mwestervelt <elle.westervelt@gmail.com>
2018-11-18 16:53:09 -0500

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,13 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+   data.each do |category, options|
+     options.each do |option, pigeons|
+       pigeons.each do |pigeon|
+         pigeon_list[pigeon] ||= {}
+         pigeon_list[pigeon][category] ||= []
+         pigeon_list[pigeon][category] << option.to_s
+    end
+  end
+end
+   pigeon_list
 end
